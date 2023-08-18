cce8d6ce0927390c3f66baf455719ca016f9fea5
kakali-here <pathakkakali12@gmail.com>
2023-08-18 13:29:00 +0530

    pointer array & pointer function

---
+++ basics/pointer_functions.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void print(int *p) {
+
+    cout << *p <<endl;
+
+}
+  
+  void update(int *p) {
+     
+     p = p + 1;
+  }
+
+int main() {
+
+    int value = 5;
+    int *p= &value;
+
+   // print(p);
+     
+
+     cout <<"Before"<<p<<endl;
+     update(p);
+     cout<<"After"<<p<<endl;
+    return 0;
+}
